237aba37d40ec02f6aa5317cec2414631217c3c4
sykang <sykang@surromind.ai>
2024-10-22 18:03:51 +0900

    Fix docker version

--- app/config/model_lite_mapper.py
+++ app/config/model_lite_mapper.py
@@ -2,4 +2,8 @@ from enum import Enum
 
 
 class ModelLiteMapper(Enum):
-    OWLV2_PTQ = "aipaas-harbor.surromind.ai/ptq-workflow/owl_v2_ptq_test:v0.4"
+    """
+    경량화 / 최적화 이미지 매핑
+    """
+
+    OWLV2_PTQ = "aipaas-harbor.surromind.ai/ptq-workflow/owl_v2_ptq_test:v0.5"
