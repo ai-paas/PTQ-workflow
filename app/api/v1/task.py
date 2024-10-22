2ac392ecf7f9490a977f62842cc557b578870953
sykang <sykang@surromind.ai>
2024-10-22 16:31:02 +0900

    Create task routers

--- app/api/v1/task.py
+++ app/api/v1/task.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.core.db.connect import SessionDepends
 from app.core.db.models.model_task import ModelTask
-from app.services.model_service import ModelService
+from app.schemas.requests.task import PatchTaskForm
 
 router = APIRouter()
 
@@ -20,11 +20,7 @@ def get_tasks(
     """
     Get all tasks
     """
-    ModelService.get_lite_model(
-        db=db,
-    )
     tasks = db.query(ModelTask).all()
-    print(tasks)
     return tasks
 
 
@@ -33,14 +29,30 @@ def get_task(*, db: Session = SessionDepends, task_id: str):
     """
     Get one task by task_id
     """
-    pass
-    return task_id
+
+    task = db.query(ModelTask).filter(ModelTask.task_uuid == task_id).first()
+    return task
 
 
 @router.patch("/{task_id}")
-def patch_task(*, db: Session = SessionDepends, task_id: str):
+def patch_task(
+    *,
+    db: Session = SessionDepends,
+    task_id: str,
+    patch_task_form: PatchTaskForm,
+):
     """
     Patch one task by task_id
     """
-    pass
-    return task_id
+
+    task = db.query(ModelTask).filter(ModelTask.task_uuid == task_id).first()
+    updates = {
+        "progress_status": patch_task_form.progress_status,
+        "model_path_output": patch_task_form.model_path_output,
+    }
+    if task:
+        for key, value in updates.items():
+            setattr(task, key, value)  # 속성 업데이트
+        db.commit()
+        db.refresh(task)
+    return task
